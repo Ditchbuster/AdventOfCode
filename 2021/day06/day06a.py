96764384f4493267f08dd3a9c93f06723179380c
Ditchbuster <trioman008@hotmail.com>
2021-12-06 00:35:02 -0600

    day 6

---
+++ 2021/day06/day06a.py
@@ -0,0 +1,12 @@
+with open('input.txt') as f:
+    init = [int(x) for x in f.readline().rstrip().split(',')]
+    count = [init.count(x) for x in range(9)]
+    days = 80
+    for d in range(days):
+        born = count[0]
+        for i in range(len(count)-1):
+            count[i] = count[i+1]
+        count[6] += born
+        count[8] = born
+        print(count)
+    print(sum(count))
