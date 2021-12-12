beea5887102e678bc9e64f18a7e9eeacca56ef71
Ditchbuster <trioman008@hotmail.com>
2021-12-12 11:18:53 -0600

    day 11

---
+++ 2021/day11/day11b.py
@@ -0,0 +1,48 @@
+with open("input.txt") as f:
+    data = f.read()
+data = [[int(c) for c in line] for line in data.splitlines()]
+# print(data)
+
+
+def printGrid(data):
+    for line in data:
+        print("".join(str(x) for x in line))
+
+
+def flash(y, x):
+    count = 0
+    for i in range(-1, 2):
+        for j in range(-1, 2):
+            if not (i == 0 and j == 0) and (
+                y + i >= 0 and y + i < len(data) and x + j >= 0 and x + j < len(data[y])
+            ):
+                data[y + i][x + j] += 1
+                if data[y + i][x + j] == 10:
+                    powered.append((y + i, x + j))
+                    count += 1
+    return count
+
+
+# printGrid(data)
+count = 0
+for step in range(1000):
+    powered = []
+    for y in range(len(data)):
+        for x in range(len(data[y])):
+            data[y][x] += 1
+            if data[y][x] == 10:
+                powered.append((y, x))
+                count += 1
+    for p in powered:
+        count += flash(p[0], p[1])
+    flashed = 0
+    for y in range(len(data)):
+        for x in range(len(data[y])):
+            if data[y][x] > 9:
+                data[y][x] = 0
+                flashed += 1
+    if flashed == len(data) * len(data[0]):
+        print(step + 1)
+        break
+    # printGrid(data)
+print(count)
